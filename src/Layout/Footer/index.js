a41c70df9a0a68409e729cc1e1df80e4a4aaa056
masoudborzouie <masoudborzouie@gmail.com>
2022-02-21 22:33:40 +0330

    commit n21

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -5,8 +5,7 @@ function Index() {
     <div className="bg-dark" style={{ height: "300px" }}>
       <h1 className=" text-white">Footer foooooter</h1>
       <p>IC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder ca</p>
+    </p>
     </div>
   );
 }
