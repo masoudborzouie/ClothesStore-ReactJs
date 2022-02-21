e89ee361cce640d5637ceeae2473fadf8ca812ff
masoudborzouie <masoudborzouie@gmail.com>
2022-02-21 09:10:08 +0330

    Halle

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -3,7 +3,10 @@ import React from "react";
 function Index() {
   return (
     <div className="bg-dark" style={{ height: "300px" }}>
-      <h1 className=" text-white">Footer RTY</h1>
+      <h1 className=" text-white">Footer foooooter</h1>
+      <p>IC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder ca</p>
     </div>
   );
 }
